1a0bc8715f46bce1241087b4183187c24d0844dc
norwich2123-star <norwich2123@gmail.com>
2026-08-26 20:28:06 +0100

    Update MainActivity.java

--- app/src/main/java/com/stevenspureclean/app/MainActivity.java
+++ app/src/main/java/com/stevenspureclean/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
@@ -18,7 +19,13 @@ public class MainActivity extends Activity {
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
 
-        webView.loadUrl("file:///android_asset/index.html");
+        webView.clearCache(true);
+        webView.clearHistory();
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/index.html?v=20");
     }
 }
