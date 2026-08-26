e21726f6166463e8680a0b5b01cafafc7c962fd2
norwich2123-star <norwich2123@gmail.com>
2026-08-26 13:59:49 +0100

    Update MainActivity.java

--- app/src/main/java/com/stevenspureclean/app/MainActivity.java
+++ app/src/main/java/com/stevenspureclean/app/MainActivity.java
@@ -1,15 +1,15 @@
 package com.stevenspureclean.app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.webkit.WebViewAssetLoader;
 import androidx.webkit.WebViewClientCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private WebView webView;
 
@@ -23,22 +23,23 @@ public class MainActivity extends AppCompatActivity {
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(false);
 
         WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
-                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
+                .addPathHandler("/assets/",
+                        new WebViewAssetLoader.AssetsPathHandler(this))
                 .build();
 
         webView.setWebViewClient(new WebViewClientCompat() {
             @Override
             public android.webkit.WebResourceResponse shouldInterceptRequest(
                     WebView view,
-                    WebResourceRequest request
-            ) {
+                    WebResourceRequest request) {
                 return assetLoader.shouldInterceptRequest(request.getUrl());
             }
         });
 
-        webView.loadUrl("https://appassets.androidplatform.net/assets/index.html");
+        webView.loadUrl(
+                "https://appassets.androidplatform.net/assets/index.html"
+        );
     }
 }
