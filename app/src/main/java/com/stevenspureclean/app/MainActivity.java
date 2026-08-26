d52597af39a5c27c95d8408cb2d755b3ac05d143
norwich2123-star <norwich2123@gmail.com>
2026-08-26 14:12:31 +0100

    Update MainActivity.java

--- app/src/main/java/com/stevenspureclean/app/MainActivity.java
+++ app/src/main/java/com/stevenspureclean/app/MainActivity.java
@@ -2,44 +2,23 @@ package com.stevenspureclean.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
-import androidx.webkit.WebViewAssetLoader;
-import androidx.webkit.WebViewClientCompat;
-
 public class MainActivity extends Activity {
-
-    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        webView = new WebView(this);
+        WebView webView = new WebView(this);
         setContentView(webView);
 
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
 
-        WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
-                .addPathHandler("/assets/",
-                        new WebViewAssetLoader.AssetsPathHandler(this))
-                .build();
-
-        webView.setWebViewClient(new WebViewClientCompat() {
-            @Override
-            public android.webkit.WebResourceResponse shouldInterceptRequest(
-                    WebView view,
-                    WebResourceRequest request) {
-                return assetLoader.shouldInterceptRequest(request.getUrl());
-            }
-        });
-
-        webView.loadUrl(
-                "https://appassets.androidplatform.net/assets/index.html"
-        );
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
